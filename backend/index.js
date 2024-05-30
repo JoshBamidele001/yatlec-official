745bdd82726ce55ca4c369f9d504bb3dda1fc0d8
Joshua Bamidele <exploits4real@gmail.com>
2024-05-30 11:56:29 +0100

    pre-installation

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from'path'
 
 dotenv.config()
 
     mongoose
     .connect(process.env.MONGO)
-    .then(()=>{
+    .then(()=>{ 
         console.log('Connected to MongoDB');
     }).catch ((err)=>{
         console.log(err);
     })
 
+    const __dirname = path.resolve();
+
+   
+
 const app = express()
+
+app.use(express.static(path.join(__dirname, '/yatlec/dist')));
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'yatlec', 'dist', 'index.html'));
+})
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
